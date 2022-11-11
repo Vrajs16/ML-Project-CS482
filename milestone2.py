970a86d4ed2623ca1fc11704b95458290bd2ad61
Vraj Shah <vrajs16108@gmail.com>
2022-11-11 18:55:21 -0500

    Added More Images

--- milestone2.py
+++ milestone2.py
@@ -22,27 +22,31 @@ def create_folders(categories):
 def get_images(categories):
     API_KEY = os.getenv("GOOGLE_API_KEY")
     SEARCH_ENGINE_ID = os.getenv("SEARCH_ENGINE_ID")
-    main_url = f"https://www.googleapis.com/customsearch/v1?key={API_KEY}&cx={SEARCH_ENGINE_ID}&searchType=image&imgSize=large&num=10&q="
 
+    main_url = f"https://www.googleapis.com/customsearch/v1?key={API_KEY}&cx={SEARCH_ENGINE_ID}&searchType=image&imgSize=large&start="
     for category in categories:
+        START = 0
         print("Category:", category)
-        category_url = f"{main_url}{category}"
-        response = requests.get(category_url)
-        if response.status_code == 200:
-            data = response.json()
-            items = data["items"]
-            urls = []
-            for item in items:
-                urls.append(item["link"])
+        for i in range(3):
+            category_url = f"{main_url}{START}&num=10&q={category}"
+            response = requests.get(category_url)
+            if response.status_code == 200:
+                data = response.json()
+                items = data["items"]
+                urls = []
+                for item in items:
+                    urls.append(item["link"])
 
-            for url in urls:
-                print("URL:", url)
-                response = requests.get(url)
-                filename = url.split("/")[-1]
-                with open(f"{category}/{filename}", "wb") as f:
-                    f.write(response.content)
-        else:
-            print(response.text)
+                for i, url in enumerate(urls):
+                    if "www.homedepot.com" not in url:
+                        print(f"\t{i} - URL:", url)
+                        response = requests.get(url)
+                        filename = url.split("/")[-1]
+                        with open(f"{category}/{filename}", "wb") as f:
+                            f.write(response.content)
+            else:
+                print(response.text)
+            START += 10
 
 
 def main():
@@ -60,19 +64,10 @@ def main():
         "Bathroom Sinks",
         "Bath Accessories",
         "Bathroom Mirrors",
-        "Bathroom Exhaust Fans",     
+        "Bathroom Fans"
     """
     product_categories: list[str] = [
-        "Bathroom Vanities",
-        "Bath Faucets",
-        "Bathtubs",
-        "Showers",
-        "Toilets",
-        "Bathroom Storage",
-        "Bathroom Sinks",
-        "Bath Accessories",
-        "Bathroom Mirrors",
-        "Bathroom Exhaust Fans",
+        "Vent Fans"
     ]
 
     # Create a folder for each product category if it does not exist
